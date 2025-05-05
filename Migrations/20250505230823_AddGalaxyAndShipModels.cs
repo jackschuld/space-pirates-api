89b63647caff3deff3cd67637edea3d5502d4583
jackschuld <schuldjack@gmail.com>
2025-05-05 16:17:26 -0700

    Setup logic to randomly generate a galaxy and save game info at start of game

---
+++ Migrations/20250505230823_AddGalaxyAndShipModels.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpacePirates.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGalaxyAndShipModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Charging",
+                table: "Shields",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Shields",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CurrentFuel",
+                table: "FuelSystems",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Charging",
+                table: "Shields");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Shields");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CurrentFuel",
+                table: "FuelSystems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+        }
+    }
+}
